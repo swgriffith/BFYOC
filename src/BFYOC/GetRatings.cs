304d10b3b81dc128a03e6159dcc2c701fa757e0e
Steve Griffith <steve.griffith@microsoft.com>
2020-10-20 13:16:05 -0400

    another commit

--- src/BFYOC/GetRatings.cs
+++ src/BFYOC/GetRatings.cs
@@ -33,12 +33,4 @@ namespace BFYOC
         }
     }
 
-    public class Rating
-    {
-        public string userId { get; set; }
-        public string productId { get; set; }
-        public string locationName { get; set; }
-        public int rating { get; set; }
-        public string userNotes { get; set; }
-    }
 }
