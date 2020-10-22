0475a9dc9c2fbcbc80403e3d39fa5fb68648ae20
Steve Griffith <steve.griffith@microsoft.com>
2020-10-22 15:17:21 -0400

    more biggererer

--- src/BFYOC/ProcessSales.cs
+++ src/BFYOC/ProcessSales.cs
@@ -36,20 +36,15 @@ namespace BFYOC
                 }
                 catch (Exception e)
                 {
-                    log.LogInformation(e.InnerException.ToString());
+                    //log.LogInformation(e.InnerException.ToString());
                     // We need to keep processing the rest of the batch - capture this exception and continue.
                     // Also, consider capturing details of the message that failed processing so it can be processed again later.
-                    exceptions.Add(e);
+                    throw e;
                 }
             }
 
             // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
 
-            if (exceptions.Count > 1)
-                throw new AggregateException(exceptions);
-
-            if (exceptions.Count == 1)
-                throw exceptions.Single();
         }
     }
 }
